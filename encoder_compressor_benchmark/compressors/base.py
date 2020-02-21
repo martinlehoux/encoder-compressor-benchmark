9913444cba3190e0de174d5f86098fadfb68c07f
Martin Lehoux <martin@lehoux.net>
2020-02-20 23:48:35 -0800

    feat - BaseCompressor

---
+++ encoder_compressor_benchmark/compressors/base.py
@@ -0,0 +1,27 @@
+import time
+from humanize import naturalsize
+
+
+class BaseCompressor:
+    _time: float = 0
+    ext: str = ''
+    _size: int = 0
+
+    def _compress(self, data: bytes) -> bytes:
+        raise NotImplementedError
+
+    def compress(self, data: bytes) -> bytes:
+        start = time.time()
+        compressed_data = self._compress(data)
+        end = time.time()
+        self._time = end - start
+        self._size = len(compressed_data)
+        return compressed_data
+
+    @property
+    def size(self) -> str:
+        return naturalsize(self._size)
+
+    @property
+    def time(self) -> str:
+        return f"{self._time * 1000:4.0f} ms"
